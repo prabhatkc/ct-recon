1ed4da2626f553c6593cbcabbab62ddb54e5fc90
prabhatkc <prabhat.kc077@gmail.com>
2023-08-01 18:21:56 -0400

    fixing plot issues

--- digiNoise/src/insert_noise_2_obj_model.m
+++ digiNoise/src/insert_noise_2_obj_model.m
@@ -69,27 +69,27 @@ function [] = insert_noise_2_obj_model(obj, obj_info, sys_info, misc_info)
     % ----------------------
     % Default paramaters
     % --------------------
-	if ~isfield(misc_info, 'plot_figures'); plot_figures='F'; 
+    if ~isfield(misc_info, 'plot_figures'); plot_figures='F'; 
     else, plot_figures=misc_info.plot_figures; end
 
-	if ~isfield(sys_info, 'nb'); NB=986; 
+    if ~isfield(sys_info, 'nb'); NB=986; 
     else, NB=sys_info.nb; end
 
-	if ~isfield(sys_info, 'na'); NA=2880; 
+    if ~isfield(sys_info, 'na'); NA=2880; 
     else, NA=sys_info.na; end
 
-	if ~isfield(sys_info, 'ds'); DS=0.95;
+    if ~isfield(sys_info, 'ds'); DS=0.95;
     else, DS= sys_info.ds; end
 
-	if ~isfield(obj_info, 'down'); DOWN=1;
+    if ~isfield(obj_info, 'down'); DOWN=1;
     else, DOWN= obj_info.down; end
 
     if ~isfield(obj_info, 'fov'); FOV=250;
     else, FOV= obj_info.fov; end
 
-	if ~isfield(sys_info, 'max_flux'); MAX_FLUX = 2.25e5;
+    if ~isfield(sys_info, 'max_flux'); MAX_FLUX = 2.25e5;
     else, MAX_FLUX=sys_info.max_flux; end    
-    
+
     if ~isfield(misc_info, 'filter_string'); filter_string = '';
     else, filter_string=misc_info.filter_string; end 
 
@@ -97,25 +97,25 @@ function [] = insert_noise_2_obj_model(obj, obj_info, sys_info, misc_info)
     % else set this equal to HU of air
     if ~isfield(misc_info, 'scalefac_k'); scalefac_k = 1024.0;
     else, scalefac_k=misc_info.scalefac_k; end 
-    
+
     if ~isfield(misc_info, 'intercept_k'); intercept_k = 0.0;
     else, intercept_k=misc_info.intercept_k; end 
 
     % This parameter is imputed from ref [2]. For ND acquisition
-    % around 200 mAs, equivalent k factor is 0.85. 
+    % around 83 mAs, equivalent k factor is 0.85. 
     if ~isfield(misc_info, 'k_nd'); k_nd = 0.85;
     else, k_nd=misc_info.k_nd; end 
-    
+
     if ~isfield (misc_info, 'noiseless'); noiseless = 'False';
     else, noiseless = misc_info.noiseless; end 
-    
+
     %
     if ~isfield(misc_info, 'out_dtype'); out_dtype = 'int16';
     else, out_dtype = misc_info.out_dtype; end
 
-	if ~exist(misc_info.output_folder, 'dir')
-	    mkdir(misc_info.output_folder)
-	end
+    if ~exist(misc_info.output_folder, 'dir')
+        mkdir(misc_info.output_folder)
+    end
     obj_info
     sys_info
     misc_info
@@ -144,66 +144,84 @@ function [] = insert_noise_2_obj_model(obj, obj_info, sys_info, misc_info)
     % scalefac_k  = 1024;
     figure (1); 
     for i=1:Nz
-	    xtrue = obj(:, :, i);
-	    xtrue = xtrue - intercept_k;
-	    xtrue = xtrue'*mu_water/scalefac_k+ mu_water;
-	    if (min(xtrue(:))<0)
-		    xtrue = xtrue + (-min(xtrue(:)));
-	    end
+        xtrue = obj(:, :, i);
+        xtrue = xtrue - intercept_k;
+        xtrue = xtrue'*mu_water/scalefac_k+ mu_water;
+        if (min(xtrue(:))<0)
+            xtrue = xtrue + (-min(xtrue(:)));
+        end
     
-	    sino_n = G*xtrue;
+        sino_n = G*xtrue;
         
         if strcmp(noiseless, 'True')
             % setting projection to be noiseless
             ldsino = sino_n;
         else 
-	        % perform low-dose simulation on the projection data
-	        ndsino  = pct_ldsino(I02, sino_n, k_nd, sig, 'T');
+            % perform low-dose simulation on the projection data
+            ndsino  = pct_ldsino(I02, sino_n, k_nd, sig, 'T');
             if ~isfield(misc_info, 'k_ld')
                 % i.e. if LD acquisition factor relative to ND is not specified
                 % then simply perform ND acquisition
                 ldsino  = ndsino;
             else
                 k_ld    = misc_info.k_ld;
-	            ldsino  = yuct_ldsino(I02, ndsino, k_ld, sig*sig);
+                ldsino  = yuct_ldsino(I02, ndsino, k_ld, sig*sig);
             end
         end 
 
-	    % reconstruct image
-	    fbp = fbp2(ldsino, tmp, 'window', filter_string); %'hanning,0.6');%hanning,0.6');
-
-	    if strcmp(plot_figures, 'T')
-		    im plc 2 2 
-		    clim = [min(xtrue(:)) max(xtrue(:))];
-		    im(1, xtrue, 'high-dose'), cbar;
-		    im(2, fbp, 'FBP low-dose'), cbar;
-		    im(3, ndsino_n, 'sino'), cbar;
-		    im(4, ndsino, 'noisy sino'), cbar;
-        end 
+        % reconstruct image
+        fbp = fbp2(ldsino, tmp, 'window', filter_string); %'hanning,0.6');%hanning,0.6');
 
         if strcmp(misc_info.norm_type, 'positive_scale')
-			if (min(fbp(:))<0)
-				fbp = fbp + (-min(fbp(:)));
-			end
+            if (min(fbp(:))<0)
+                fbp = fbp + (-min(fbp(:)));
+            end
         elseif strcmp(misc_info.norm_type, 'remove_negative')
-			if (min(fbp(:))<0)
-				neg_ind = find(fbp<0);
-				fbp(neg_ind) =0.0;
+            if (min(fbp(:))<0)
+                neg_ind = find(fbp<0);
+                fbp(neg_ind) =0.0;
             end
         else 
             fbp;
-		end
-	    xtrue = (xtrue' - mu_water)*scalefac_k/mu_water + intercept_k;
-	    fbp   = (fbp' - mu_water)*scalefac_k/mu_water + intercept_k;
-	    % fbp   = imresize(fbp, 0.8);
-	    imshow(fbp, []); colorbar;
-        
-        % default dtype of output is int16
-        if strcmp(out_dtype, 'uint16')
-	        write_raw_img([misc_info.output_folder,'/fbp_sharp_', num2str(i), '.raw'], uint16(fbp), 'uint16');
-        else
-            write_raw_img([misc_info.output_folder,'/fbp_sharp_', num2str(i), '.raw'], int16(fbp), 'int16');
         end
+        xtrue = (xtrue' - mu_water)*scalefac_k/mu_water + intercept_k;
+        fbp   = (fbp' - mu_water)*scalefac_k/mu_water + intercept_k;
+        % imshow(fbp, []); colorbar;
+        
+        if strcmp(plot_figures, 'T')
+            if strcmp(noiseless, 'True')
+                im plc 1 3
+                im(1, xtrue, 'ground truth'), cbar;
+                im(2, sino_n, 'noiseless sino'), cbar;
+                im(3, fbp, 'noiseless FBP'), cbar;
+                colormap gray;
+            else
+                if ~isfield(misc_info, 'k_ld')
+                    im plc 1 3
+                    im(1, xtrue, 'ground truth'), cbar;
+                    im(2, ldsino, 'normal-dose sino'), cbar;
+                    im(3, fbp, 'normal-dose FBP'), cbar;
+                    colormap gray;
+                else
+                    im plc 1 3
+                    im(1, xtrue, 'ground truth'), cbar;
+                    im(2, ldsino, 'low-dose sino'), cbar;
+                    im(3, fbp, 'low-dose FBP'), cbar;
+                    colormap gray;
+                end 
+            end 
+        end 
+
+         
+
+        if ~isempty(misc_info.output_folder)
+            % default dtype of output is int16
+            if strcmp(out_dtype, 'uint16')
+                write_raw_img([misc_info.output_folder,'/fbp_sharp_', num2str(i), '.raw'], uint16(fbp), 'uint16');
+            else
+                write_raw_img([misc_info.output_folder,'/fbp_sharp_', num2str(i), '.raw'], int16(fbp), 'int16');
+            end
+        end 
     end
     
 end 
